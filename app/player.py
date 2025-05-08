a75467f61bf033a780d116f2663ff3cbfb2ccd46
Sparrows <35826584+sparrowsaurora@users.noreply.github.com>
2025-05-08 20:21:27 +0800

    added score property, comparison ops and it's tests

--- app/player.py
+++ app/player.py
@@ -1,7 +1,8 @@
 class Player:
-    def __init__(self, unique_id: str, player_name: str):
+    def __init__(self, unique_id: str, player_name: str, score: int =0):
         self.__uid = unique_id
         self.__player_name = player_name
+        self.__score = score
 
     def uid(self):
         return self.__uid
@@ -9,8 +10,15 @@ class Player:
     def name(self):
         return self.__player_name
 
+    def set_score(self, score: int):
+        self.__score = score
+
+    @property
+    def score(self):
+        return self.__score
+
     def __str__(self):
-        return f"Player({self.name()}, {self.uid()})"
+        return f"Player({self.name()}, {self.uid()}, Score = {self.__score})"
 
     @classmethod
     def hash(cls, key: str) -> int:
@@ -20,4 +28,17 @@ class Player:
         return Player.hash(self.__uid)
 
     def __eq__(self, other) -> bool:
-        return self.uid() == other.uid()
+        return self.score == other.score
+
+    def __ge__(self, other) -> bool:
+        return self.score >= other.score
+
+    def __gt__(self, other) -> bool:
+        return self.score > other.score
+
+    def __le__(self, other) -> bool:
+        return self.score <= other.score
+
+    def __lt__(self, other) -> bool:
+        return self.score < other.score
+
--- test/player_test.py
+++ test/player_test.py
@@ -12,4 +12,20 @@ class TestPlayer(unittest.TestCase):
         player = Player("1", "Jane Doe")
         self.assertEqual(player.name(), "Jane Doe")
 
+    def test_score(self):
+        player = Player("1", "John Doe", 1)
+        self.assertEqual(player.score, 1)
+
+    def test_comparison(self):
+        player = Player("1", "John Doe", 1)
+        player2 = Player("2", "Jane Doe")
+        self.assertEqual(player.score, 1)
+        self.assertEqual(player2.score, 0)
+        self.assertFalse(player.score == player2.score)
+        self.assertTrue(player.score > player2.score)
+        self.assertFalse(player.score < player2.score)
+        self.assertNotEqual(player.score, player2.score)
+        player3 = Player("3", "example", 1)
+        self.assertEqual(player.score, player3.score)
+
 unittest.main()
